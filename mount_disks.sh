50012020435a474cb5c98938ed482268d7d87dba
Henrique Goulart <henriquedsg89@gmail.com>
2021-09-10 15:11:41 -0300

    Dynamic mount disks script for emulab

---
+++ mount_disks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+disks=`lsblk | grep ^sd | grep -v sda | awk '{print $1}'` 
+i=0
+
+for disk in $disks
+do
+    echo "Mounting disk $disk"
+    sudo mkdir /checkpoint$i/ 
+    sudo chmod 777 /checkpoint$i/	
+    sudo mkfs.ext4 /dev/$disk
+    sudo mount /dev/$disk /checkpoint$i/
+    sudo mkdir /checkpoint$i/states/
+    sudo mkdir /checkpoint$i/metadata/
+    sudo chmod 777 -R /checkpoint$i/
+    ((i++))
+done
